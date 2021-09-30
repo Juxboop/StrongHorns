909c9cddb1e6d3578f0335ea7134787d2e1b33be
phipham0 <90461213+phipham0@users.noreply.github.com>
2021-09-29 22:12:36 -0500

    Made blastzones, added double jump, fall thru platofrms

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1600, 900, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() 
 {
     game.load.image('deathscreen', 'assets/deathscreen.png');
     game.load.image('winscreen', 'assets/winscreen.png');
     game.load.image('ground', 'assets/platform.png');
-    game.load.image('tower', 'assets/utTower.png');
+    game.load.image('tower', 'assets/utTower.jpg');
     game.load.image('football', 'assets/football.png');
     game.load.spritesheet('hookem', 'assets/fullruncycle.png', 64, 64);
 
@@ -13,6 +13,7 @@ function preload()
 
 var player;
 var platforms;
+var stages;
 var cursors;
 var bounds;
 var footballs;
@@ -20,6 +21,7 @@ var score = 0;
 var scoreText;
 var gameover = false;
 var lastLeft = true;
+var jumpCounter = 0;
 
 function create() 
 {
@@ -28,48 +30,66 @@ function create()
 
     bounds = game.add.group();
     bounds.enableBody = true;
-    var bound = bounds.create(0, game.world.height - 600, 'ground');
-    bound.scale.setTo(2,0.5)
+    
+    //top blast zone
+    var bound = bounds.create(0, game.world.height - 1000, 'ground');
+    bound.scale.setTo(5,0.01)
     bound.body.immovable = true;
     
-    bound = bounds.create(0, game.world.height + 50, 'ground');
-    bound.scale.setTo(2,0.5)
+    //bottom blast zone
+    bound = bounds.create(0, game.world.height + 100, 'ground');
+    bound.scale.setTo(5,0.01)
     bound.body.immovable = true;
     
-    bound = bounds.create(0, game.world.height + 50, 'ground');
-    bound.scale.setTo(2,0.5)
+    //left blast zone
+    bound = bounds.create(-100, 0, 'ground');
+    bound.scale.setTo(0.01, 23)
     bound.body.immovable = true;
-    //bound.angle += 1;
+    
+    //right blast zone
+    bound = bounds.create(1700, 0, 'ground');
+    bound.scale.setTo(0.01, 23)
+    bound.body.immovable = true;
     
     
     
-    platforms = game.add.group();
-    platforms.enableBody = true;
-    var ground = platforms.create(0, game.world.height, 'ground');
-    ground.scale.setTo(1.5, 4);
+    stages = game.add.group();
+    stages.enableBody = true;
+    
+    var ground = stages.create(0, game.world.height - 150, 'ground');
+    ground.scale.setTo(2, 4);
     ground.anchor.set(0.5);
     ground.x = game.world.centerX;
     ground.body.immovable = true;
     
     
-    var ledge = platforms.create(450, 400, 'ground');
+    platforms = game.add.group();
+    platforms.enableBody = true;
+    
+    var ledge = platforms.create(game.world.centerX - 200, 500, 'ground');
     ledge.body.immovable = true;
-    ledge.scale.setTo(0.4, 0.5)
+    ledge.scale.setTo(0.4, 0.3)
+    ledge.anchor.set(0.5);
 
     
-    ledge = platforms.create(175, 400, 'ground');
+    ledge = platforms.create(game.world.centerX + 200, 500, 'ground');
     ledge.body.immovable = true;
-    ledge.scale.setTo(0.4, 0.5)
+    ledge.scale.setTo(0.4, 0.3)
+    ledge.anchor.set(0.5);
     
-    ledge = platforms.create(315, 300, 'ground');
+    ledge = platforms.create(game.world.centerX, 350, 'ground');
     ledge.body.immovable = true;
-    ledge.scale.setTo(0.4, 0.5)
+    ledge.scale.setTo(0.4, 0.3)
+    ledge.anchor.set(0.5);
     
-    player = game.add.sprite(game.world.centerX, game.world.height - 200, 'hookem');
+    player = game.add.sprite(game.world.centerX, game.world.height - 300, 'hookem');
     player.scale.setTo(1.25, 1.25);
     game.physics.arcade.enable(player);
-    player.body.gravity.y = 400;
+    player.body.gravity.y = 1000;
     player.body.collideWorldBounds = false;
+    
+    //game.physics.add.collide(player, platforms);
+
     
     player.animations.add('right', [0, 1, 2, 3, 4], 10, true);
     player.animations.add('left', [5, 6, 7, 8, 9], 10, true);
@@ -86,7 +106,7 @@ function create()
     
     
     
-    scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#ff0000' });
+    scoreText = game.add.text(16, 16, 'Score: ', { fontSize: '32px', fill: '#ff0000' });
     cursors = game.input.keyboard.createCursorKeys();
     
 }
@@ -96,7 +116,7 @@ function update()
     
     if (gameover == false)
     {
- 
+        var hitStage = game.physics.arcade.collide(player, stages);
         var hitPlatform = game.physics.arcade.collide(player, platforms);
         game.physics.arcade.collide(footballs, platforms);
 
@@ -132,15 +152,35 @@ function update()
                 player.frame = 0;
             }
         }
-
-        if (cursors.up.isDown && player.body.touching.down && hitPlatform)
+        
+        if (player.body.touching.down && (hitPlatform || hitStage) && jumpCounter > 1)
         {
-            player.body.velocity.y = -350;
+            jumpCounter = 0;
         }
+        
+        if (cursors.up.isDown && player.body.touching.down && (hitPlatform || hitStage))
+        {
+            player.body.velocity.y = -650;
+            jumpCounter += 1;
+            game.add.text(16, 16, 'Score: ' + jumpCounter, { fontSize: '32px', fill: '#ff0000' });
+        }
+        
+        if (cursors.up.isDown && jumpCounter <= 10)
+        {
+            player.body.velocity.y = -650;
+            jumpCounter += 1;   
+        }
+        
         
         if (cursors.down.isDown)
         {
-            player.body.velocity.y = 350;
+            player.body.velocity.y = 550;
+        }
+        
+        if (cursors.down.isDown && player.body.touching.down && hitPlatform)
+        {
+            player.position.y = player.position.y + 15;
+            player.body.velocity.y = -650;
         }
         
     }
@@ -168,11 +208,15 @@ function gotKilled ()
     //game.add.text(16, 16, 'Score: ' + score, { fontSize: '32px', fill: '#ff0000' });
   
 }
+/*
+function upInputReleased ()
+{
+    var released = false;
 
-//function outOfBounds ()
-//{
-//    if (player.gameObject === this)
-        
-//}
+    released = this.input.keyboard.upDuration(Phaser.Keyboard.UP);
+    released |= this.game.input.activePointer.justReleased();
 
+    return released;
+}
+*/
 
